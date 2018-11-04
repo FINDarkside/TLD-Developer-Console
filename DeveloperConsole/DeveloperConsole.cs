14a8d6f03bd4a0229b37d7070d922fd4dfc2c54f
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2018-11-04 16:00:52 +0100

    Add pos to get position, tp to teleport

--- DeveloperConsole/DeveloperConsole.cs
+++ DeveloperConsole/DeveloperConsole.cs
@@ -25,6 +25,10 @@ namespace DeveloperConsole
             uConsole.RegisterCommand("currentSceneName", () => Debug.Log(Scene.SceneManager.GetActiveScene().name));
 
             uConsole.RegisterCommand("currentSceneIndex", () => Debug.Log(Scene.SceneManager.GetActiveScene().buildIndex));
+
+            uConsole.RegisterCommand("pos", GetPosition);
+
+            uConsole.RegisterCommand("tp", Teleport);
         }
 
         private static void LoadScene()
@@ -44,6 +48,49 @@ namespace DeveloperConsole
                 FlyMode.Enter();
             else
                 FlyMode.TeleportPlayerAndExit();
+        }
+
+        private static void GetPosition()
+        {
+            Vector3 pos = GameManager.GetVpFPSPlayer().transform.position;
+            Debug.LogFormat("[{0:F2} / {1:F2} / {2:F2}]", pos.x, pos.y, pos.z);
+        }
+
+        private static void Teleport()
+        {
+            Vector3 target;
+
+            if (uConsole.GetNumParameters() < 2)
+            {
+                Debug.Log("Usage: tp x z    or    tp x y z.\nExample: tp 123 890");
+                return;
+            }
+            else if (uConsole.GetNumParameters() == 2)
+            {
+                float x = uConsole.GetFloat();
+                float z = uConsole.GetFloat();
+
+                Vector3 start = new Vector3(x, 10000f, z);
+                if (Physics.Raycast(start, Vector3.down, out RaycastHit raycastHit, float.PositiveInfinity, Utils.m_PhysicalCollisionLayerMask | 1048576 | 134217728))
+                {
+                    target = raycastHit.point + new Vector3(0, 0.01f, 0);
+                }
+                else
+                {
+                    target = new Vector3(x, 0, z);
+                }
+            }
+            else
+            {
+                float x = uConsole.GetFloat();
+                float y = uConsole.GetFloat();
+                float z = uConsole.GetFloat();
+                target = new Vector3(x, y, z);
+            }
+
+            Quaternion rot = GameManager.GetVpFPSCamera().transform.rotation;
+            GameManager.GetPlayerManagerComponent().TeleportPlayer(target, rot);
+            GameManager.GetPlayerManagerComponent().StickPlayerToGround();
         }
     }
 }
